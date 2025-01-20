b0aa6119710e6827839fb583a6c7e02ef043528c
rathitanishk <tanishkrathi9000@gmail.com>
2025-01-20 10:16:15 +0530

    Commit

---
+++ 11 Week WorkShop on Data Structure and Algorithms/Mathematics/Count.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the digit:";
+    cin>>n;
+    int c=0;
+    while (n>0){
+        n=n/10;
+        c=c+1;
+    }
+    cout<<c;
+}
